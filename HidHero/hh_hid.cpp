443564e1422db0c3b13ee918f3116d9d16904b96
RSKraken <83251705+RSKraken@users.noreply.github.com>
2022-02-05 15:25:07 -0500

    gay fmt

--- HidHero/hh_hid.cpp
+++ HidHero/hh_hid.cpp
@@ -7,88 +7,88 @@
 
 namespace hh
 {
-    std::wstring HidDevice::get_product()
-    {
-        BOOLEAN success;
-        WCHAR stringBuf[128];
+	std::wstring HidDevice::get_product()
+	{
+		BOOLEAN success;
+		WCHAR stringBuf[128];
 
-        success = HidD_GetProductString(h, stringBuf, sizeof(stringBuf));
-        return success ? std::wstring(stringBuf) : L"N/A";
-    }
+		success = HidD_GetProductString(h, stringBuf, sizeof(stringBuf));
+		return success ? std::wstring(stringBuf) : L"N/A";
+	}
 
-    std::wstring HidDevice::get_manufacturer()
-    {
-        BOOLEAN success;
-        WCHAR stringBuf[128];
+	std::wstring HidDevice::get_manufacturer()
+	{
+		BOOLEAN success;
+		WCHAR stringBuf[128];
 
-        success = HidD_GetManufacturerString(h, stringBuf, sizeof(stringBuf));
-        return success ? std::wstring(stringBuf) : L"N/A";
-    }
+		success = HidD_GetManufacturerString(h, stringBuf, sizeof(stringBuf));
+		return success ? std::wstring(stringBuf) : L"N/A";
+	}
 
-    std::wstring HidDevice::get_serial()
-    {
-        BOOLEAN success;
-        WCHAR stringBuf[128];
+	std::wstring HidDevice::get_serial()
+	{
+		BOOLEAN success;
+		WCHAR stringBuf[128];
 
-        success = HidD_GetSerialNumberString(h, stringBuf, sizeof(stringBuf));
-        return success ? std::wstring(stringBuf) : L"N/A";
-    }
+		success = HidD_GetSerialNumberString(h, stringBuf, sizeof(stringBuf));
+		return success ? std::wstring(stringBuf) : L"N/A";
+	}
 
 	std::vector<HidDevice> Hid::list()
 	{
-        GUID guid;
-        HidD_GetHidGuid(&guid);
+		GUID guid;
+		HidD_GetHidGuid(&guid);
 
-        CONFIGRET status = CR_SUCCESS;
-        PWSTR deviceInterfaceList = NULL;
-        ULONG deviceInterfaceListLength = 0;
-        PWSTR currentInterface;
-        HANDLE deviceHandle = INVALID_HANDLE_VALUE;
-        WCHAR stringBuf[128];
-        std::vector<HidDevice> devices;
+		CONFIGRET status = CR_SUCCESS;
+		PWSTR deviceInterfaceList = NULL;
+		ULONG deviceInterfaceListLength = 0;
+		PWSTR currentInterface;
+		HANDLE deviceHandle = INVALID_HANDLE_VALUE;
+		WCHAR stringBuf[128];
+		std::vector<HidDevice> devices;
 
-        status = CM_Get_Device_Interface_List_Size(&deviceInterfaceListLength, &guid, NULL, CM_GET_DEVICE_INTERFACE_LIST_PRESENT);
-        if (status != CR_SUCCESS)
-        {
-            goto ret;
-        }
+		status = CM_Get_Device_Interface_List_Size(&deviceInterfaceListLength, &guid, NULL, CM_GET_DEVICE_INTERFACE_LIST_PRESENT);
+		if (status != CR_SUCCESS)
+		{
+			goto ret;
+		}
 
-        if (deviceInterfaceListLength <= 1)
-        {
-            goto ret;
-        }
+		if (deviceInterfaceListLength <= 1)
+		{
+			goto ret;
+		}
 
-        deviceInterfaceList = (PWSTR)malloc(deviceInterfaceListLength * sizeof(WCHAR));
-        if (deviceInterfaceList == NULL)
-        {
-            goto ret;
-        }
+		deviceInterfaceList = (PWSTR)malloc(deviceInterfaceListLength * sizeof(WCHAR));
+		if (deviceInterfaceList == NULL)
+		{
+			goto ret;
+		}
 
-        ZeroMemory(deviceInterfaceList, deviceInterfaceListLength * sizeof(WCHAR));
+		ZeroMemory(deviceInterfaceList, deviceInterfaceListLength * sizeof(WCHAR));
 
-        status = CM_Get_Device_Interface_List(&guid, NULL, deviceInterfaceList, deviceInterfaceListLength, CM_GET_DEVICE_INTERFACE_LIST_PRESENT);
-        if (status != CR_SUCCESS)
-        {
-            goto ret;
-        }
+		status = CM_Get_Device_Interface_List(&guid, NULL, deviceInterfaceList, deviceInterfaceListLength, CM_GET_DEVICE_INTERFACE_LIST_PRESENT);
+		if (status != CR_SUCCESS)
+		{
+			goto ret;
+		}
 
-        for (currentInterface = deviceInterfaceList; *currentInterface; currentInterface += wcslen(currentInterface) + 1)
-        {
-            deviceHandle = CreateFile(currentInterface, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
-            if (deviceHandle == INVALID_HANDLE_VALUE)
-            {
-                continue;
-            }
+		for (currentInterface = deviceInterfaceList; *currentInterface; currentInterface += wcslen(currentInterface) + 1)
+		{
+			deviceHandle = CreateFile(currentInterface, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+			if (deviceHandle == INVALID_HANDLE_VALUE)
+			{
+				continue;
+			}
 
-            devices.push_back(HidDevice(deviceHandle));
-        }
+			devices.push_back(HidDevice(deviceHandle));
+		}
 
-    ret:
-        if (deviceInterfaceList)
-        {
-            free(deviceInterfaceList);
-        }
+	ret:
+		if (deviceInterfaceList)
+		{
+			free(deviceInterfaceList);
+		}
 
-        return devices;
+		return devices;
 	}
 }
